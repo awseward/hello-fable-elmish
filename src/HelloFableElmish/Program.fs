7267a42d934ee7c96919b38b284bf55c3c035846
Drew Seward <awswrd@gmail.com>
2018-02-25 18:10:07 -0800

    chore: Implement basics example
    https://fable-elmish.github.io/elmish/basics.html

--- src/HelloFableElmish/Program.fs
+++ src/HelloFableElmish/Program.fs
@@ -2,7 +2,36 @@
 
 open System
 
+type Model = { x: int }
+
+type Msg =
+| Increment
+| Decrement
+
+open Elmish
+
+let init () =
+  { x = 0}, Cmd.ofMsg Increment
+
+let update msg model =
+  match msg with
+  | Increment when model.x < 3 ->
+      { model with x = model.x + 1 }, Cmd.ofMsg Increment
+
+  | Increment ->
+      { model with x = model.x + 1 }, Cmd.ofMsg Decrement
+
+  | Decrement when model.x > 0 ->
+      { model with x = model.x - 1 }, Cmd.ofMsg Decrement
+
+  | Decrement ->
+      { model with x = model.x - 1 }, Cmd.ofMsg Increment
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  Program.mkProgram init update (fun model _ -> printf "%A\n" model)
+  |> Program.run
+  |> ignore
+
+  Console.ReadLine()
+  0
